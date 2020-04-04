7136b511fa17c008897bf362b4a7460647f912f5
Alex-Anh-9x <63136388+Alex-Anh-9x@users.noreply.github.com>
2020-04-04 04:18:48 +0000

    test commit

---
+++ Test.cs
@@ -0,0 +1,10 @@
+using System;
+class AlexTest
+{
+    public static void main(string[] args)
+    {
+        int age = 23;
+        string name = "Alex";
+        System.Console.WriteLine("My name is {0} and I'm {1} years old!", name, age);
+    }
+}
